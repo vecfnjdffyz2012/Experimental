3eb2e8899ae7cf2914b416595782e1e6cf51eaf0
DASHIE <DASHIE@DESKTOP-N402VB3>
2020-01-28 21:06:48 +0300

    Новая строка

--- lab 10/Program.cs
+++ lab 10/Program.cs
@@ -11,6 +11,7 @@ namespace lab_10
         static void Main(string[] args)
         {
             Console.WriteLine("Перове знакомство с GitHub");
+            Console.WriteLine("Проверка изменений на сервере");
         }
     }
 }
